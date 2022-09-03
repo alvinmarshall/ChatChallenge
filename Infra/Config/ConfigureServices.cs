e4f4383ee26bf20fb7f5c771d39e8c69ab305f55
Bik-Krlvn <kelvinbirikorang@gmail.com>
2022-09-03 01:18:59 +0000

    add configuration

---
+++ Infra/Config/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Repository;
+using Infra.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Config;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IChatRoomRepository, ChatRoomRepository>();
+        services.AddScoped<IChatMessageRepository, MessageRepository>();
+        return services;
+    }
+}
