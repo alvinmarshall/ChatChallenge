4067d9e6932857032883a5597d969cbfe2ef0d30
Bik-Krlvn <kelvinbirikorang@gmail.com>
2022-09-16 18:12:19 +0000

    update file

--- Bot/Models/Stock.cs
+++ Bot/Models/Stock.cs
@@ -2,12 +2,12 @@ namespace Bot.Models;
 
 public record Stock
 {
-    public string Symbol { get; set; }
-    public string Date { get; set; }
-    public string Time { get; set; }
-    public string Open { get; set; }
-    public string High { get; set; }
-    public string Low { get; set; }
-    public string Close { get; set; }
-    public string Volume { get; set; }
+    public string? Symbol { get; set; }
+    public string? Date { get; set; }
+    public string? Time { get; set; }
+    public string? Open { get; set; }
+    public string? High { get; set; }
+    public string? Low { get; set; }
+    public string? Close { get; set; }
+    public string? Volume { get; set; }
 }
